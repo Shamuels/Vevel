a3f0977c2e90040a2fc4ad6cb9849f741f50c051
Shamuel <horaceshamuel@gmail.com>
2024-05-20 22:08:19 -0400

    Added preliminary version of character tracking

--- out/extension.js
+++ out/extension.js
@@ -24,23 +24,40 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = __importStar(require("vscode"));
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 function activate(context) {
+    let char_number = 0;
+    const fs = require('fs');
+    fs.writeFile('Output.txt', "test");
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "level" is now active!');
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
+    //
     let disposable = vscode.commands.registerCommand('level.helloWorld', () => {
         // The code you place here will be executed every time your command is executed
         // Display a message box to the user
         vscode.window.showInformationMessage('Hello World from vscode-level!');
     });
+    vscode.workspace.onDidChangeTextDocument((e) => {
+        char_number++;
+        console.log(char_number);
+    });
+    vscode.workspace.onDidSaveTextDocument((e) => {
+        console.log("saved");
+    });
+    var nuts = vscode.window.createTerminal("Nuts");
+    /*
+        vscode.window.onDidCloseTerminal((e:vscode.Terminal) => {
+            console.log("closed");
+            }
+    
+        );
+    */
     context.subscriptions.push(disposable);
 }
 exports.activate = activate;
