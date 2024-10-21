e94c442677490b9884e9f50ac3e8a6f981d08b3d
Horace Samuels <horaceshamuel@gmail.com>
2024-10-21 16:18:51 -0400

    Restructured code for sanity Added functionality for commits to increase exp (thank god) Fixed bug where holding down save or any key would just constantly generate exp

--- out/extension.js
+++ out/extension.js
@@ -27,7 +27,11 @@ exports.deactivate = exports.activate = void 0;
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = __importStar(require("vscode"));
-//Note: Look into using await for thenables 
+const node_fs_1 = require("node:fs");
+const node_fs_2 = require("node:fs");
+//TODO
+//get debouncing working for commit function and saving
+//look into exp being gained from git commit
 //My extension will be running while someone is doing other things on their pc so it needs to be asynchronous or else its gonna hitch them
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -36,13 +40,39 @@ function activate(context) {
     let max_exp;
     let current_lvl;
     let filepathuri = vscode.Uri.file('/Users/kami/vscode-level/test.txt');
+    const workspacefolder = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
     const encoder = new TextEncoder();
     const decoder = new TextDecoder();
     const wsedit = new vscode.WorkspaceEdit;
-    let a = 0;
+    const status_bar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 50);
+    const gitExtension = vscode.extensions.getExtension('vscode.git')?.exports;
+    const git = gitExtension?.getAPI(1);
+    //need to confirm that the extension is enabled and initalized before creating listener
+    //can do this through then statements and so on
+    async function getCommit() {
+        const commit = await git?.repositories[0].log();
+        if (commit != undefined && workspacefolder != undefined) {
+            const topdir = commit[0].hash.substring(0, 2);
+            const subdir = commit[0].hash.substring(2);
+            if ((0, node_fs_2.existsSync)(workspacefolder + "//.git//objects//" + topdir + "//" + subdir)) {
+                current_exp += 20;
+                status_bar.text = `LVL ${current_lvl} ${current_exp}/${max_exp}`;
+                status_bar.command = 'level.helloWorld';
+                status_bar.show();
+            }
+        }
+    }
+    function debounce_inherit(func, ms) {
+        let timeout;
+        return function () {
+            clearTimeout(timeout);
+            timeout = setTimeout(func, ms);
+        };
+    }
+    const debounce_commit = debounce_inherit(getCommit, 1000);
+    const debounce_exp = debounce_inherit(increaseExp, 70);
     //Note: Make save only a few seconds after on document save has been activated
     //Create level bar
-    const status_bar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 50);
     //Create initial file for storage of exp and lvl
     //Set default lvl and exp
     vscode.workspace.fs.stat(filepathuri).then(statFulfilled, statRejected);
@@ -65,11 +95,18 @@ function activate(context) {
     //adds 1 for text written in the document
     //add +2 changes for different actions like saves and undos (fun randomizer element)
     vscode.workspace.onDidChangeTextDocument((e) => {
-        increaseExp();
+        //debounce_exp()
     });
     vscode.workspace.onDidSaveTextDocument((e) => {
-        increaseExp();
+        debounce_exp();
     });
+    if (workspacefolder != undefined) {
+        (0, node_fs_1.watch)(workspacefolder + "//.git//COMMIT_EDITMSG", (e) => {
+            if (gitExtension?.enabled == true && git?.state == "initialized") {
+                debounce_commit();
+            }
+        });
+    }
     function increaseExp() {
         current_exp++;
         status_bar.text = `LVL ${current_lvl} ${current_exp}/${max_exp}`;
@@ -87,7 +124,9 @@ function activate(context) {
         vscode.workspace.fs.writeFile(filepathuri, lvl_information);
         vscode.workspace.applyEdit(wsedit);
     }
-    setInterval(saveData, 10000);
+    //Interval will eventually be replaced with debouncing so no more 
+    //spamming the disk even if the user isnt doing anything
+    setInterval(saveData, 20000);
     context.subscriptions.push(status_bar);
 }
 exports.activate = activate;
--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,17 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { GitExtension, Repository } from './git';
+import { watch } from 'node:fs';
+import { existsSync } from 'node:fs';
+import { constrainedMemory } from 'process';
+import { Console } from 'console';
+import { time } from 'node:console';
 
+//TODO
+//get debouncing working for commit function and saving
+//look into exp being gained from git commit
 
-//Note: Look into using await for thenables 
 //My extension will be running while someone is doing other things on their pc so it needs to be asynchronous or else its gonna hitch them
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -12,17 +20,19 @@ export function activate(context: vscode.ExtensionContext) {
 	let max_exp: number;
 	let current_lvl: number;
 	let filepathuri = vscode.Uri.file('/Users/kami/vscode-level/test.txt');
-	const encoder = new TextEncoder();
+	const workspacefolder = vscode.workspace.workspaceFolders?.[0].uri.fsPath
+	const encoder = new TextEncoder()
 	const decoder = new TextDecoder()
 	const wsedit = new vscode.WorkspaceEdit
-	let a = 0
-	//Note: Make save only a few seconds after on document save has been activated
-
-	//Create level bar
 	const status_bar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 50);
+	const gitExtension = vscode.extensions.getExtension<GitExtension>('vscode.git')?.exports;
+	const git = gitExtension?.getAPI(1);
+	const debounce_commit = debounce_inherit(getCommit, 1000)
+	const debounce_exp = debounce_inherit(increaseExp, 70)
 
-	//Create initial file for storage of exp and lvl
-	//Set default lvl and exp
+	//Uses metadata to identify whether a file exists or not (could just use file exists but whatever)
+	//If fulfilled grab info from file and display them on level bar
+	//If rejected base level stats are set for level bar and saved to file
 	vscode.workspace.fs.stat(filepathuri).then(statFulfilled, statRejected)
 
 	function statFulfilled() {
@@ -45,21 +55,53 @@ export function activate(context: vscode.ExtensionContext) {
 		status_bar.show();
 	}
 
-
-	//Provide experience for typing
-	//adds 1 for text written in the document
-	//add +2 changes for different actions like saves and undos (fun randomizer element)
+	//Provides experience mainly for typing but also gives exp for other changes in the doc
 	vscode.workspace.onDidChangeTextDocument((e: vscode.TextDocumentChangeEvent) => {
-		increaseExp()
+		debounce_exp()
 	});
 
-
+	//Provides experience for saving document
 	vscode.workspace.onDidSaveTextDocument((e: vscode.TextDocument) => {
-		increaseExp()
+		debounce_exp()
 	})
 
+	//Watches COMMIT_EDITMSG for changes to see if commit is made
+	//If changes are made to COMMIT_EDITMSG commit is verified through a debounce function and exp is provided to level bar
+	if (workspacefolder != undefined) {
+		watch(workspacefolder + "//.git//COMMIT_EDITMSG", (e: any) => {
+			if (gitExtension?.enabled == true && git?.state == "initialized") {
+				debounce_commit()
+			}
+		})
+	}
+
+	async function getCommit() {
+		const commit = await git?.repositories[0].log()
+		if (commit != undefined && workspacefolder != undefined) {
+			const topdir = commit[0].hash.substring(0, 2)
+			const subdir = commit[0].hash.substring(2)
+			if (existsSync(workspacefolder + "//.git//objects//" + topdir + "//" + subdir)) {
+				current_exp += 20
+				status_bar.text = `LVL ${current_lvl} ${current_exp}/${max_exp}`;
+				status_bar.command = 'level.helloWorld';
+				status_bar.show();
+			}
+		}
+	}
 
 
+	//Base debounce function that can be inherited by other custom debounces
+	function debounce_inherit(func: any, ms: number) {
+		let timeout: any
+		return function () {
+			clearTimeout(timeout)
+			timeout = setTimeout(func, ms)
+		}
+	}
+
+
+
+	//Base function is called to increase a user's exp
 	function increaseExp() {
 		current_exp++
 		status_bar.text = `LVL ${current_lvl} ${current_exp}/${max_exp}`;
@@ -73,15 +115,16 @@ export function activate(context: vscode.ExtensionContext) {
 
 	}
 
-	//Occasionally stores current level to a text file
-
+	//Converts level data to a valid form before saving to txt file
 	function saveData() {
 		let lvl_information: Uint8Array = encoder.encode([current_lvl, current_exp, max_exp].toString());
 		vscode.workspace.fs.writeFile(filepathuri, lvl_information)
 		vscode.workspace.applyEdit(wsedit)
 	}
 
-	setInterval(saveData, 10000);
+
+	setInterval(saveData, 20000);
+
 
 
 
